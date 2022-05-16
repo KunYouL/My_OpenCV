4c7eb8a08d3b8492dbf7568fbd1f9e609e4eff5c
KunYou <4a8g0107@stust.edu.tw>
2022-05-16 21:11:44 +0800

    My_opencv

---
+++ My_openCv1.py
@@ -0,0 +1,192 @@
+import PIL.Image
+from PIL import Image, ImageTk
+import tkinter as tk
+import cv2 as cv
+from tkinter import filedialog
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+
+
+class Myfunction:
+    def open_file1(self):
+        filetypes = (
+            ('png file', '*.png'),
+            ('jpg file', '*.jpg'),
+            ('All file', '*.*')
+        )
+        filename = filedialog.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes
+        )
+        img = cv.imread(filename)
+        return img
+
+    def open_file(self):
+        filetypes = (
+            ('png file', '*.png'),
+            ('jpg file', '*.jpg'),
+            ('All file', '*.*')
+        )
+        filename = filedialog.askopenfilename(
+            title='Open a file1',
+            initialdir='/',
+            filetypes=filetypes
+        )
+        img = cv.imread(filename)
+        cv.imshow('img', img)
+        cv.waitKey()
+
+    def save_file(self):
+        pass
+
+    def gray(self):  #灰階
+        src = self.open_file1()
+        gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+        cv.imshow('gray', gray)
+        cv.waitKey()
+
+    def HSV(self):  #HSV
+        src = self.open_file1()
+        HSV = cv.cvtColor(src, cv.COLOR_BGR2HSV)
+        cv.imshow('HSV', HSV)
+        cv.waitKey()
+
+    def histogram(self):  #直方圖
+        scr = self.open_file1()
+        cv.imshow('Original image', scr)
+        color = ('b', 'g', 'r')
+        for i, col in enumerate(color):
+            hist = cv.calcHist([scr], [i], None, [256], [0, 256])
+            plt.plot(hist, color=col)
+            plt.xlim([0, 256])
+        plt.show()
+
+    def hist_equa(self):  # 直方圖等化
+        src = self.open_file1()
+        gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+        img_eq = cv.equalizeHist(gray)
+        cv.imshow('equalized image', img_eq)
+        plt.hist(img_eq.ravel(), 256, [0, 256])
+        plt.show()
+
+    def canny_detector(self):  #邊緣檢測器
+        max_lowThreshold = 100
+        window_name = 'Edge Map'
+        title_trackbar = 'Min Threshold:'
+        ratio = 3
+        kernel_size = 3
+
+        #the callback function for trackbar
+        def canny_value_change(val):
+            low_threshold = val
+            img_blur = cv.blur(gray, (3, 3))
+            detected_edge = cv.Canny(img_blur, low_threshold, low_threshold * ratio, kernel_size)
+            mask = detected_edge != 0
+            dst = src * (mask[:, :, None].astype(src.dtype))
+            cv.imshow(window_name, dst)
+
+        src = self.open_file1()
+        gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+        cv.namedWindow(window_name)
+        cv.createTrackbar(title_trackbar, window_name, 0, max_lowThreshold, canny_value_change)
+        canny_value_change(0)  #先給數值
+        cv.waitKey()
+
+    def hough_transform(self):  #霍夫轉換
+        src = cv.cvtColor(self.open_file1(), cv.COLOR_BGR2GRAY)  #先轉灰階
+        dst = cv.Canny(src, 50, 200, None, 3)  #零界點，sobel孔大小
+
+        #Copy edges to the image that will display the results in BGR
+        cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)   #轉BGR
+        cdstP = np.copy(cdst)
+
+        lines = cv.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)  #1=距離 150=門檻值 0=預設值
+
+        if lines is not None:
+            for i in range(0, len(lines)):
+                rho = lines[i][0][0]  #算每條線的ab值
+                theta = lines[i][0][1]
+                a = math.cos(theta)
+                b = math.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+                pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+                cv.line(cdst, pt1, pt2, (0, 0, 255), 3, cv.LINE_AA)
+
+        linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
+
+        if linesP is not None:
+            for i in range(0, len(linesP)):
+                l = linesP[i][0]
+                cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255),  3, cv.LINE_AA)
+
+        cv.imshow("Source", src)
+        cv.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)  #標轉霍夫變換
+        cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)  #概率線變換
+
+    def gaussianBlur(self): #高斯模糊
+        src = self.open_file1()
+        Blur = cv.GaussianBlur(src, (15, 15), 10)   #kernel只能是奇數，且逗號前後數值要相同
+        cv.imshow('Blur', Blur)
+
+    def corner_harris(self): #邊角檢測
+        src = self.open_file1()
+        gray= cv.cvtColor(src,cv.COLOR_BGR2GRAY)
+        gray = np.float32(gray)
+        dst = cv.cornerHarris(gray,2,3,0.06)
+        dst = cv.dilate(dst,None)
+        src[dst>0.01*dst.max()] = [0,0,255]
+        cv.imshow('src',src)
+        cv.imshow('dst',dst)
+
+    def thresholding(self):  #二值化
+        src = self.open_file1()
+        src = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+        ret, th1 = cv.threshold(src, 127, 255, cv.THRESH_BINARY)  #二值化(未模糊)
+        #adaptiveThreshold可將一般圖片做自適應二值化
+        th2 = cv.adaptiveThreshold(src, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2) #  平均二值化(未模糊降噪)
+        th3 = cv.adaptiveThreshold(src, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2) #高斯二值化(未模糊醬噪)
+
+        cv.imshow('th1', th1)
+        cv.imshow('th2', th2)
+        cv.imshow('th3', th3)
+
+    def transpose(self):  #水平翻轉
+        src = self.open_file1()
+        cv.imshow('src', src)
+        new_src = cv.flip(src, 1)
+        cv.imshow('new_src', new_src)
+
+    def warpperspective(self):  #影像透視
+        src = self.open_file1()
+        rows,cols=src.shape[:2]
+        print(rows,cols)
+        pts1=np.float32([[0,0],[274,0],[0,184],[274,184]])
+        pts2=np.float32([[30,50],[rows-30,50],[30,cols-50],[rows-30,cols-50]])
+        M=cv.getPerspectiveTransform(pts1,pts2)
+        dst=cv.warpPerspective(src,M,(cols,rows))
+        cv.imshow('src',src)
+        cv.imshow('dst1',dst)
+        pass
+
+    def affine(self):  #仿射轉換(平移)
+        src = self.open_file1()
+        height, width = src.shape[:2]  #讀取原影像的長、寬
+        x = 30  #自訂義轉換矩陣M的x軸移動值
+        y = 20  #自訂義轉換矩陣M的y軸移動值
+        M = np.float32([[1, 0, x], [0, 1, y]])
+        move = cv.warpAffine(src, M, (width, height))   #平移映射
+        cv.imshow('src', src)
+        cv.imshow('move', move)
+
+    def rotate(self):  #仿射轉換(旋轉)
+        src = self.open_file1()
+        height, width = src.shape[:2] #讀取原影像的長、寬
+        M=cv.getRotationMatrix2D((width/2,height/2),45,0.6)  #以中心為原點，逆時針轉45度，且縮小為原圖的0.6倍
+        rotate=cv.warpAffine(src,M,(width,height))   #旋轉映射
+        cv.imshow('src', src)
+        cv.imshow('rotate', rotate)
---
+++ main.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+import PIL.Image
+import numpy as np
+from PIL import ImageTk, Image
+import cv2 as cv
+from tkinter import filedialog
+from matplotlib import pyplot as plt
+import My_openCv1
+
+class App:
+    def __init__(self, window, window_title):
+        self.window = window
+        self.window.title('My window')
+        self.window.geometry('1000x600')
+
+        self.my_function = My_openCv1.Myfunction()
+
+        #Add a menubar
+        self.main_menu = tk.Menu(window)  #創建視窗
+
+        #Add file submenu
+        self.file_menu = tk.Menu(self.main_menu, tearoff=0)
+        self.file_menu.add_command(label="開啟檔案", command=self.my_function.open_file)
+        self.file_menu.add_command(label="儲存檔案", command=self.my_function.save_file)
+        self.file_menu.add_separator()  #分隔線
+        self.file_menu.add_command(label="離開程式", command=window.quit)
+
+        #Add operation1 submenu
+        self.operation1_menu = tk.Menu(self.main_menu, tearoff=0)
+        self.operation1_menu.add_command(label='邊緣檢測器', command=self.my_function.canny_detector)
+        self.operation1_menu.add_command(label='霍夫轉換', command=self.my_function.hough_transform)
+        self.operation1_menu.add_command(label='高斯模糊', command=self.my_function.gaussianBlur)
+        self.operation1_menu.add_command(label='邊角偵測', command=self.my_function.corner_harris)
+
+        #Add operation2 submenu
+        self.operation2_menu = tk.Menu(self.main_menu, tearoff=0)
+        self.operation2_menu.add_command(label='灰階圖', command=self.my_function.gray)
+        self.operation2_menu.add_command(label='HSV', command=self.my_function.HSV)
+        self.operation2_menu.add_command(label='水平翻轉', command=self.my_function.transpose)
+
+        # Add operation3 submenu
+        self.operation3_menu = tk.Menu(self.main_menu, tearoff=0)
+        self.operation3_menu.add_command(label='直方圖', command=self.my_function.histogram)
+        self.operation3_menu.add_command(label='直方圖等化', command=self.my_function.hist_equa)
+        self.operation3_menu.add_command(label='二值化', command=self.my_function.thresholding)
+
+        # Add operation4 submenu
+        self.operation4_menu = tk.Menu(self.main_menu, tearoff=0)
+        self.operation4_menu.add_command(label='影像投影', command=self.my_function.warpperspective)
+        self.operation4_menu.add_command(label='仿射轉換(平移)', command=self.my_function.affine)
+        self.operation4_menu.add_command(label='仿射轉換(旋轉)', command=self.my_function.rotate)
+
+        #Add  submenu to mainmenu
+        self.main_menu.add_cascade(label="檔案", menu=self.file_menu)
+        self.main_menu.add_cascade(label='功能1', menu=self.operation1_menu)
+        self.main_menu.add_cascade(label='功能2', menu=self.operation2_menu)
+        self.main_menu.add_cascade(label='功能3', menu=self.operation3_menu)
+        self.main_menu.add_cascade(label='功能4', menu=self.operation4_menu)
+
+        #display menu
+        self.window.config(menu=self.main_menu)
+        self.window.mainloop()
+
+
+App(tk.Tk(), "OpenCv with Tkinter GUI")
+
